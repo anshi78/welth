796e01b60e031533e83a6ce4609330a27c2a0d59
Anshu <rastogianshika05@gmail.com>
2025-11-14 16:17:32 +0530

    FIX: middleware.js

--- middleware.js
+++ middleware.js
@@ -1,23 +1,22 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+// Define the routes that are protected
 const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/account(.*)",
   "/transaction(.*)",
 ]);
 
+// This is the main middleware function
 export default clerkMiddleware((auth, req) => {
+  // If the route is a protected route, protect it
   if (isProtectedRoute(req)) {
     auth().protect();
   }
 });
 
+// This config specifies which routes the middleware runs on
 export const config = {
-  // This matcher tells the middleware to run on all routes
-  // EXCEPT for routes that start with:
-  // - /api (API routes)
-  // - /_next/static (static files)
-  // - /_next/image (image optimization files)
-  // - /favicon.ico (favicon file)
+.
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
